127879e54b635b7d0c55597d8fa3c8b208f563dc
Budr <bwb934@gmail.com>
2025-09-27 18:13:38 +0300

    Initial Commit

---
+++ lazygym/lazygymApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazygymApp.swift
+//  lazygym
+//
+//  Created by Budr Albakri on 27.09.25.
+//
+
+import SwiftUI
+
+@main
+struct lazygymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
